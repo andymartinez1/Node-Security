382e7a44f9dc8535eb5b72c9832224044c5930d7
Andrew Martinez <andy72391@gmail.com>
2024-07-13 01:20:49 -0400

    Added client-side cookie based authentication and sessions. Added functionality to the isLoggedIn function using passport and sessions. Styling soon to come!

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cookieSession = require("cookie-session");
 const express = require("express");
 const fs = require("fs");
 const helmet = require("helmet");
@@ -14,6 +15,8 @@ const PORT = 3000;
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -29,14 +32,36 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
+// Save the session to cookie using id for much smaller cookie size
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Read (load) the session from the cookie
+passport.deserializeUser((id, done) => {
+  //   User.findById(id).then((user) => {
+  //     done(null, user);
+  //   });
+  done(null, id);
+});
+
 const app = express();
 
-// Uses helmet request header security before any routes and Google OAuth
+// Middleware for helmet request header security, cookie sessions and Google OAuth
 app.use(helmet());
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 app.use(passport.initialize());
+app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true;
+  console.log("Current user is:", req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
   if (!isLoggedIn)
     return res.status(401).json({
       error: "You must log in",
@@ -58,7 +83,7 @@ app.get(
   passport.authenticate("google", {
     failureRedirect: "/failure",
     successRedirect: "/",
-    session: false,
+    session: true, // Set to true by default
   }),
   (req, res) => {
     console.log("Google callback");
@@ -66,10 +91,13 @@ app.get(
 );
 
 // Logout
-app.get("/auth/logout", (req, res) => {});
+app.get("/auth/logout", (req, res) => {
+  req.logout();
+  return res.redirect("/");
+});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
-  return res.send("Secret information");
+  return res.send("SECRET INFORMATION");
 });
 
 app.get("failure", (req, res) => {
